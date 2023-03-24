593a359b9ade38b2bf54f5ee8f4728f5eb2b87c9
Charles Anderson <charles.anderson.1172@gmail.com>
2023-03-23 21:26:26 -0400

    This is charlie's submission to make sure my clone works

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -3,8 +3,10 @@ package edu.wpi.fishfolk;
 public class Main {
 
   public static void main(String[] args) {
+    System.out.println("Charlie is Here\n");
     Fapp.launch(Fapp.class, args);
   }
+
 
   // shortcut: psvm
 
