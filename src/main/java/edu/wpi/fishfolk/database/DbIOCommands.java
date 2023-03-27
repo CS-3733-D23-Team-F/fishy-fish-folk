b27d07c3df743be4efc4d534b8588b474e028870
Charles Anderson <charles.anderson.1172@gmail.com>
2023-03-27 12:19:47 -0400

    CGA, latest version of DbIOCommands.java just before I switch into the specialized feature branches

--- src/main/java/edu/wpi/fishfolk/database/DbIOCommands.java
+++ src/main/java/edu/wpi/fishfolk/database/DbIOCommands.java
@@ -1,4 +1,20 @@
 package edu.wpi.fishfolk.database;
+//TODO: make this work with bash or WinCMD prompt
+//TODO: In GIT PUT THIS INTO YOUR OWN FEATURE BRANCH along with the other features you are working on
+/*TODO: Implement these commands
+    "help"
+    "exit" - This should exit to the CLI
+    "remove"
+    "insert"
+*/
 
-public class DbIOCommands {
-}
+
+
+/**
+ * @author: Charles Anderson
+ * @Updated: 3/26/2023
+ * @desc: The Database CLI interface.
+ */
+
+
+public class DbIOCommands {}
