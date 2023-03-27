f887071a0870d8d2e750b37f0326f65d4450e790
Kayos <knpham@wpi.edu>
2023-03-27 10:20:42 -0400

    Created home page, changed default size to 1440x1020 in all fxml files. Added navigation screens in Screen.java All buttons are working, added a back button and some dummy text in Signage.fxml

--- src/main/java/edu/wpi/fishfolk/controllers/SignageController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/SignageController.java
@@ -1,3 +1,15 @@
 package edu.wpi.fishfolk.controllers;
 
-public class SignageController {}
+import edu.wpi.fishfolk.navigation.Navigation;
+import edu.wpi.fishfolk.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class SignageController {
+  @FXML MFXButton backButton;
+
+  @FXML
+  public void initialize() {
+    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+  }
+}
