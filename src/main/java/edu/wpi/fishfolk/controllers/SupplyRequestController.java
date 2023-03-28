d53caa7f65bf2e4edc9e97d6a374a8f50f9d7d29
TYoutz <tbyoutz@wpi.edu>
2023-03-28 00:21:03 -0400

    submit form button goes home

--- src/main/java/edu/wpi/fishfolk/controllers/SupplyRequestController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/SupplyRequestController.java
@@ -7,9 +7,11 @@ import javafx.fxml.FXML;
 
 public class SupplyRequestController {
   @FXML MFXButton cancelButton;
+  @FXML MFXButton supplySubmitButton;
 
   @FXML
   public void initialize() {
     cancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    supplySubmitButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
   }
 }
