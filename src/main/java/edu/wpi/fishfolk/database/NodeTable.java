16404a2b55bd89443339dc40972a1997daf58ec2
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2023-03-27 22:22:32 -0400

    cleanup

--- src/main/java/edu/wpi/fishfolk/database/NodeTable.java
+++ src/main/java/edu/wpi/fishfolk/database/NodeTable.java
@@ -3,49 +3,34 @@ package edu.wpi.fishfolk.database;
 import edu.wpi.fishfolk.pathfinding.Node;
 import java.util.ArrayList;
 import java.util.LinkedList;
+
 public class NodeTable {
 
     public ArrayList<String> headers;
 
     public Node getNode(String id) {
-
-
         return null;
     }
 
     //true if inserted, false if duplicate or otherwise not added
-    public boolean insertNode(Node node){
+    public boolean insertNode(Node node) {
         return false;
     }
 
     //true if updated (maybe inserted new node) , false
-    public boolean updateNode(Node node){
-
-
-
+    public boolean updateNode(Node node) {
         return false;
+    }
 
+    public void removeNode(Node node) {
 
     }
 
-
-    public void removeNode(Node node){
-
-    }
-
-
-    public void importCSV(String fileName){
+    public void importCSV(String fileName) {
 
     }
 
-    public void exportCSV(String filename){
+    public void exportCSV(String filename) {
 
     }
-
-
-
-
-
-
-
 }
