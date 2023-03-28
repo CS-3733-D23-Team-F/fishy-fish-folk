28b9565de05d9e84b6db964fb35dab7ecb26a11a
TEspelien <trajanespelien@gmail.com>
2023-03-28 02:46:37 -0400

    barebones edge class needed for edgeTable

--- src/main/java/edu/wpi/fishfolk/pathfinding/Edge.java
+++ src/main/java/edu/wpi/fishfolk/pathfinding/Edge.java
@@ -1,3 +1,12 @@
 package edu.wpi.fishfolk.pathfinding;
 
-public class Edge {}
+public class Edge {
+
+    String node1id, node2id;
+
+    public Edge(String node1id, String node2id) {
+        this.node1id=node1id;
+        this.node2id=node2id;
+    }
+
+}
