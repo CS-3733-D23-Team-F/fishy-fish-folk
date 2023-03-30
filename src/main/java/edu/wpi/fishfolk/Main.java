184b806e33e57f1eef49b69ed5a22a246b31cfef
TEspelien <trajanespelien@gmail.com>
2023-03-29 21:40:30 -0400

    removed db connection and fixed file paths for jar

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -1,15 +1,9 @@
 package edu.wpi.fishfolk;
 
-import edu.wpi.fishfolk.database.EdgeTable;
-import edu.wpi.fishfolk.database.Fdb;
-import edu.wpi.fishfolk.database.NodeTable;
-import edu.wpi.fishfolk.pathfinding.Edge;
 import edu.wpi.fishfolk.pathfinding.Graph;
 import edu.wpi.fishfolk.pathfinding.Node;
 import edu.wpi.fishfolk.pathfinding.NodeType;
 import java.io.*;
-import java.sql.Connection;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.LinkedList;
 import javafx.geometry.Point2D;
@@ -20,82 +14,9 @@ public class Main {
 
     // Fapp.launch(Fapp.class, args);
 
-    try {
+    Graph graph = loadMapFromCSV();
 
-      System.out.println("\n--- ESTABLISHING DATABASE CONNECTION ---\n");
-
-      Fdb fdb = new Fdb();
-      Connection db = fdb.connect("teamfdb", "teamf", "teamf60");
-      db.setSchema("test");
-      System.out.println("[Main]: Current schema: " + db.getSchema() + ".");
-
-      System.out.println("\n--- TESTING NODE TABLE ---\n");
-
-      NodeTable nodeTable = new NodeTable(db, "nodetable");
-      if (fdb.createTable(db, nodeTable.getTableName())) {
-        nodeTable.addHeaders();
-      }
-
-      nodeTable.importCSV();
-
-      Node existingNode = nodeTable.getNode("CCONF001L1");
-      Node newNode =
-          new Node(
-              "CDEPT999L1",
-              new Point2D(1980, 844),
-              "L1",
-              "Tower",
-              NodeType.DEPT,
-              "Day Surgery Family Waiting Floor L1",
-              "Department C002L1");
-      Node newNodeUpdated =
-          new Node(
-              "CDEPT999L1",
-              new Point2D(1980, 844),
-              "L2",
-              "Space",
-              NodeType.DEPT,
-              "Night Surgery Family Waiting Floor L1",
-              "Department C002L1");
-
-      nodeTable.insertNode(existingNode);
-      nodeTable.insertNode(newNode);
-
-      nodeTable.updateNode(newNodeUpdated);
-
-      nodeTable.removeNode(existingNode);
-
-      nodeTable.exportCSV();
-
-      System.out.println("\n--- TESTING EDGE TABLE ---\n");
-
-      EdgeTable edgeTable = new EdgeTable(db, "edgetable");
-      if (fdb.createTable(db, edgeTable.getTableName())) {
-        edgeTable.addHeaders();
-      }
-
-      edgeTable.importCSV();
-      // edgeTable.testQuery();
-
-      Edge existingEdge = edgeTable.getEdge("CCONF002L1_WELEV00HL1");
-
-      Edge newEdge = new Edge("CDEPT999L1_CDEPT999L1", "CDEPT002L1", "CDEPT003L1");
-      Edge newEdgeUpdated = new Edge("CDEPT999L1_CDEPT999L1", "CDEPT002L1AAA", "CDEPT003L1AAA");
-
-      edgeTable.insertEdge(existingEdge);
-      edgeTable.insertEdge(newEdge);
-
-      edgeTable.updateEdge(newEdgeUpdated);
-
-      edgeTable.removeEdge(existingEdge);
-
-      edgeTable.exportCSV();
-
-      fdb.disconnect(db);
-
-    } catch (SQLException e) {
-      System.out.println(e.getMessage());
-    }
+    System.out.println(graph.AStar("fREST004L1", "fREST003L1"));
   }
 
   public static Graph loadMapFromCSV() {
@@ -110,7 +31,9 @@ public class Main {
         new HashMap<>(); // key: nodeType + floor, value is count
 
     try (BufferedReader br =
-        new BufferedReader(new FileReader("src/main/resources/edu/wpi/fishfolk/csv/L1Nodes.csv"))) {
+        new BufferedReader(
+            new FileReader(
+                "C:\\Users\\Trajan Espelien\\Documents\\GitHub\\fishy-fish-folk\\src\\main\\resources\\edu\\wpi\\fishfolk\\csv\\L1Nodes.csv"))) {
 
       String line = br.readLine(); // ignore column headers which are on the first line
       while ((line = br.readLine()) != null) {
@@ -167,7 +90,9 @@ public class Main {
     }
 
     try (BufferedReader br =
-        new BufferedReader(new FileReader("src/main/resources/edu/wpi/fishfolk/csv/L1Edges.csv"))) {
+        new BufferedReader(
+            new FileReader(
+                "C:\\Users\\Trajan Espelien\\Documents\\GitHub\\fishy-fish-folk\\src\\main\\resources\\edu\\wpi\\fishfolk\\csv\\L1Edges.csv"))) {
 
       String line = br.readLine(); // ignore column headers which are on the first line
       while ((line = br.readLine()) != null) {
