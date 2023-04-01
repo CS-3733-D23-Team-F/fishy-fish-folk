8fe56e285ff09d741bfd1c43d6e8160fb0ed2825
Kayos <knpham@wpi.edu>
2023-04-01 14:19:23 -0400

    Working on implementing a sidebar - Progress for commit #1: Can successfully close the bar

--- src/main/java/edu/wpi/fishfolk/controllers/HomeController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/HomeController.java
@@ -3,7 +3,11 @@ package edu.wpi.fishfolk.controllers;
 import edu.wpi.fishfolk.navigation.Navigation;
 import edu.wpi.fishfolk.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class HomeController {
 
@@ -15,11 +19,49 @@ public class HomeController {
 
   @FXML MFXButton officeNav;
 
+  @FXML MFXButton sideBar;
+
+  @FXML AnchorPane slider;
+
   @FXML
   public void initialize() {
     // navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
     signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
     mealNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FOOD_ORDER_REQUEST));
     officeNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SUPPLIES_REQUEST));
+
+    sideBar.setOnMouseClicked(
+        event -> {
+          TranslateTransition slide = new TranslateTransition();
+          slide.setDuration(Duration.seconds(0.4));
+          slide.setNode(slider);
+          slide.setToX(0);
+          slide.play();
+
+          slider.setTranslateX(-300);
+
+          slide.setOnFinished(
+              (ActionEvent e) -> {
+                //  sideBar.setVisible(true);
+
+              });
+        });
+
+    sideBar.setOnMouseClicked(
+        event -> {
+          TranslateTransition slide = new TranslateTransition();
+          slide.setDuration(Duration.seconds(0.4));
+          slide.setNode(slider);
+          slide.setToX(-300);
+          slide.play();
+
+          slider.setTranslateX(0);
+
+          slide.setOnFinished(
+              (ActionEvent e) -> {
+                // sideBar.setVisible(true);
+
+              });
+        });
   }
 }
