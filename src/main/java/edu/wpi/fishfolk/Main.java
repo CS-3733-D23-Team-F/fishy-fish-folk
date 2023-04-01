418716b05ff6968662a033b4452ff8374562216e
Byrnezy <97705645+Byrnezy@users.noreply.github.com>
2023-04-01 17:56:20 -0400

    created a table and a function to display the aspects of the table

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -1,6 +1,7 @@
 package edu.wpi.fishfolk;
 
 import edu.wpi.fishfolk.database.*;
+import edu.wpi.fishfolk.database.Fdb;
 import edu.wpi.fishfolk.pathfinding.*;
 
 public class Main {
@@ -8,5 +9,9 @@ public class Main {
   public static void main(String[] args) {
 
     Fapp.launch(Fapp.class, args); // run ui
+
+    Fdb fdb = new Fdb(); // Create fdb object and connect to db
+    fdb.runTests(); // Test add, remove, update of nodes and edges
+    fdb.startCLI(); // Start the command line interface for database interaction
   }
 }
