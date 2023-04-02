c314fbcbfdeee85b0ac207f406b17654caef4abe
Byrnezy <97705645+Byrnezy@users.noreply.github.com>
2023-04-02 15:14:56 -0400

    Map page changed name

--- src/main/java/edu/wpi/fishfolk/controllers/MapEditorController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/MapEditorController.java
@@ -1,7 +1,7 @@
 package edu.wpi.fishfolk.controllers;
 
 import edu.wpi.fishfolk.pathfinding.Node;
-import java.net.URL;
+import edu.wpi.fishfolk.pathfinding.NodeType;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -9,23 +9,27 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
-public class MapdataTable {
+public class MapEditorController {
   @FXML private TableView<Node> table;
   @FXML private TableColumn<Node, String> id;
   @FXML private TableColumn<Node, String> x;
   @FXML private TableColumn<Node, String> y;
   @FXML private TableColumn<Node, String> floor;
   @FXML private TableColumn<Node, String> building;
+  @FXML private TableColumn<Node, String> date;
+  @FXML private TableColumn<Node, NodeType> type;
   @FXML private TableColumn<Node, String> longName;
   @FXML private TableColumn<Node, String> shortName;
 
-  public void initialize(URL url, Node node) {
+  public void initialize(Node node) {
     // sets up the columns in the table
-    id.setCellValueFactory(new PropertyValueFactory<Node, String>("id"));
-    x.setCellValueFactory(new PropertyValueFactory<Node, String>("x"));
-    y.setCellValueFactory(new PropertyValueFactory<Node, String>("y"));
+    id.setCellValueFactory(new PropertyValueFactory<Node, String>("ID"));
+    x.setCellValueFactory(new PropertyValueFactory<Node, String>("X"));
+    y.setCellValueFactory(new PropertyValueFactory<Node, String>("Y"));
     floor.setCellValueFactory(new PropertyValueFactory<Node, String>("floor"));
     building.setCellValueFactory(new PropertyValueFactory<Node, String>("building"));
+    date.setCellValueFactory(new PropertyValueFactory<Node, String>("Date"));
+    type.setCellValueFactory(new PropertyValueFactory<Node, NodeType>("type"));
     longName.setCellValueFactory(new PropertyValueFactory<Node, String>("longName"));
     shortName.setCellValueFactory(new PropertyValueFactory<Node, String>("shortName"));
 
