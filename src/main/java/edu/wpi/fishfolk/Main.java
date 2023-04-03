ddcd6eb1ed285fa8de4291c6c7e3b8761c906b92
TEspelien <trajanespelien@gmail.com>
2023-04-03 15:03:45 -0400

    cleaned up main, added commented out section to import csvs

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -14,6 +14,10 @@ public class Main {
 
     // Fapp.launch(Fapp.class, args); // run ui
 
+    /*
+
+    set up database connection and nodeTable,  edgeTable variables
+
     Fdb fdb = new Fdb();
     try {
       fdb.db.setSchema("proto2db");
@@ -27,9 +31,36 @@ public class Main {
         new ArrayList<>(List.of("node1", "node2")), new ArrayList<>(List.of("String", "String")));
     edgeTable.init(false);
 
+     */
+
+
+
+    /*
+
+    import CSVs into 3 subtables and edge
+
+    nodeTable.importCSV(
+        "src/main/resources/edu/wpi/fishfolk/csv/MicroNode.csv",
+        "src/main/resources/edu/wpi/fishfolk/csv/Location.csv",
+        "src/main/resources/edu/wpi/fishfolk/csv/Move.csv");
+
+
+    edgeTable.importCSV("src/main/resources/edu/wpi/fishfolk/csv/Edge.csv");
+
+     */
+
+
+
+
+    /*
+
+    create and speed test graph
+
     Graph g = new Graph(nodeTable, edgeTable);
     g.populate();
 
     g.speedTest(500, false);
+
+     */
   }
 }
