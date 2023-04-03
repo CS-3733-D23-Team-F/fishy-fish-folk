1a9a360383ddb138fe8d18458d95f475799d182d
Samuel Rooney <the.sam.rooney@gmail.com>
2023-04-02 20:46:17 -0400

    add homepage button to navigate to Map Editor

--- src/main/java/edu/wpi/fishfolk/navigation/Screen.java
+++ src/main/java/edu/wpi/fishfolk/navigation/Screen.java
@@ -6,7 +6,8 @@ public enum Screen {
   SERVICE_REQUEST("views/ServiceRequest.fxml"),
   SIGNAGE("views/Signage.fxml"),
   FOOD_ORDER_REQUEST("views/FoodOrder.fxml"),
-  SUPPLIES_REQUEST("views/SupplyRequest.fxml");
+  SUPPLIES_REQUEST("views/SupplyRequest.fxml"),
+  MAP_EDITOR("views/MapEditor.fxml");
 
   private final String filename;
 
