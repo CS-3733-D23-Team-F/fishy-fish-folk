e2e4973a4d79488f9d267e29e58530e50f4993e3
Kayos <knpham@wpi.edu>
2023-04-04 15:28:46 -0400

    Instructions work properly. Can now change the attribute of the lines
    TODO: Implement multifloor pathfinding

--- src/main/java/edu/wpi/fishfolk/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/PathfindingController.java
@@ -15,7 +15,10 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
+import javafx.scene.text.Text;
 
 public class PathfindingController extends AbsController {
   @FXML MFXButton submitBtn;
@@ -26,6 +29,8 @@ public class PathfindingController extends AbsController {
   @FXML AnchorPane mapAnchor;
   @FXML ImageView mapImg;
   @FXML MFXComboBox<String> selectFloor;
+
+  @FXML Text directionInstructions;
 
   Graph graph;
   Path path;
@@ -104,28 +109,46 @@ public class PathfindingController extends AbsController {
         event -> {
           path = graph.AStar(start, end);
           drawPath(path);
+          directionInstructions.setText("Instructions: \n\n" + path.getDirections());
         });
   }
 
   private void drawPath(Path path) {
 
     LinkedList<Line> segments = new LinkedList<>();
-
+    Circle circle = new Circle();
     ArrayList<Point2D> points = path.getPoints();
 
     for (int i = 1; i < points.size(); i++) {
       segments.add(line(points.get(i - 1), points.get(i)));
+      if (i + 1 == points.size()) {
+        circle.setRadius(9);
+        circle.setFill(Color.TRANSPARENT);
+        circle.setStroke(Color.RED);
+        Line temp = line(points.get(i - 1), points.get(i));
+        circle.setLayoutX(temp.getEndX());
+        circle.setLayoutY(temp.getEndY());
+      }
     }
 
     // segments.add(new Line(0, 7, 1120, 787)); //diagonal from top left to bottom right
 
     mapAnchor.getChildren().addAll(segments);
+    mapAnchor.getChildren().add(circle);
   }
 
   private Line line(Point2D p1, Point2D p2) {
     p1 = convert(p1);
     p2 = convert(p2);
     return new Line(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+  }
+
+  public static void wait(int ms) {
+    try {
+      Thread.sleep(ms);
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
+    }
   }
 
   public Point2D convert(Point2D p) {
