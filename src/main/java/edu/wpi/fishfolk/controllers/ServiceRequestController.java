cbb5fddfdcac9249aaf74ba065a4931adcdfff5a
Khoi Pham <tuankhoi0206@gmail.com>
2023-04-10 17:11:23 -0400

    Service Request hub

--- src/main/java/edu/wpi/fishfolk/controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/ServiceRequestController.java
@@ -1,16 +1,9 @@
 package edu.wpi.fishfolk.controllers;
 
-import edu.wpi.fishfolk.navigation.Navigation;
-import edu.wpi.fishfolk.navigation.Screen;
-import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
 public class ServiceRequestController {
 
-  @FXML MFXButton backButton;
-
   @FXML
-  public void initialize() {
-    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
-  }
+  public void initialize() {}
 }
