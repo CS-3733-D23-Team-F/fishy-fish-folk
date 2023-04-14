f3461f23d6d5c3aedfe84c0888a56bae29824a5c
ChristianR839 <46659572+ChristianR839@users.noreply.github.com>
2023-04-13 21:57:46 -0400

    cleaned main

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -24,35 +24,5 @@ public class Main {
     fdb.removeEntry(new Node(9999, new Point2D(500, 600), "uhh, dirt?", "bingus"));
 
     fdb.nodeTable.updateDatabase();
-
-    // set up database connection and nodeTable,  edgeTable variables
-
-    // Fdb fdb = new Fdb();
-    // fdb.loadTablesFromCSV();
-
-    /*
-
-    import CSVs into 3 subtables and edge
-
-    nodeTable.importCSV(
-        "src/main/resources/edu/wpi/fishfolk/csv/MicroNode.csv",
-        "src/main/resources/edu/wpi/fishfolk/csv/Location.csv",
-        "src/main/resources/edu/wpi/fishfolk/csv/Move.csv", false);
-
-
-    edgeTable.importCSV("src/main/resources/edu/wpi/fishfolk/csv/Edge.csv", false);
-
-     */
-
-    /*
-
-    create and speed test graph
-
-    Graph g = new Graph(nodeTable, edgeTable);
-    g.populate();
-
-    g.speedTest(500, false);
-
-     */
   }
 }
