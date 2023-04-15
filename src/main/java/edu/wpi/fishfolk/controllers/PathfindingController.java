1fcbdcb1279ef3f4aec007f1961043a5950d51c9
Khoi Pham <tuankhoi0206@gmail.com>
2023-04-15 15:44:02 -0400

    move navigation bar to root and have everything resizable in relation to how big the screen resolution is

--- src/main/java/edu/wpi/fishfolk/controllers/PathfindingController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/PathfindingController.java
@@ -1,7 +1,5 @@
 package edu.wpi.fishfolk.controllers;
 
-import edu.wpi.fishfolk.navigation.Navigation;
-import edu.wpi.fishfolk.navigation.Screen;
 import edu.wpi.fishfolk.pathfinding.*;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
@@ -10,13 +8,10 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import javafx.animation.Interpolator;
-import javafx.animation.TranslateTransition;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
@@ -24,18 +19,6 @@ import net.kurobako.gesturefx.GesturePane;
 
 public class PathfindingController extends AbsController {
 
-  @FXML MFXButton signageNav;
-  @FXML MFXButton mealNav;
-  @FXML MFXButton supplyNav;
-  @FXML MFXButton pathfindingNav;
-  @FXML MFXButton mapEditorNav;
-  @FXML AnchorPane serviceBar;
-  @FXML MFXButton serviceNav;
-
-  @FXML MFXButton exitButton;
-
-  @FXML MFXButton closeServiceNav;
-  @FXML AnchorPane slider;
   @FXML Text floorDisplay;
 
   @FXML MFXFilterComboBox<String> startSelector;
@@ -73,14 +56,6 @@ public class PathfindingController extends AbsController {
   private void initialize() {
 
     // buttons to other pages
-    viewFood.setOnMouseClicked(event -> Navigation.navigate(Screen.VIEW_FOOD_ORDERS));
-    viewSupply.setOnMouseClicked(event -> Navigation.navigate(Screen.VIEW_SUPPLY_ORDERS));
-    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
-    mealNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FOOD_ORDER_REQUEST));
-    furnitureNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FURNITURE_REQUEST));
-    mapEditorNav.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
-    pathfindingNav.setOnMouseClicked(event -> Navigation.navigate(Screen.PATHFINDING));
-    exitButton.setOnMouseClicked(event -> System.exit(0));
 
     zoomIn.setOnMouseClicked(
         event -> {
@@ -96,58 +71,6 @@ public class PathfindingController extends AbsController {
           pane.animate(Duration.millis(200))
               .interpolateWith(Interpolator.EASE_BOTH)
               .zoomBy(zoom - 0.2, new Point2D(bounds.getCenterX(), bounds.getCenterY()));
-        });
-
-    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
-    // mealNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FOOD_ORDER_REQUEST));
-    // officeNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SUPPLIES_REQUEST));
-    mapEditorNav.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
-    pathfindingNav.setOnMouseClicked(event -> Navigation.navigate(Screen.PATHFINDING));
-    exitButton.setOnMouseClicked(event -> System.exit(0));
-
-    closeServiceNav.setVisible(false);
-    closeServiceNav.setDisable(true);
-
-    serviceNav.setOnMouseClicked(
-        event -> {
-          serviceBar.setVisible(true);
-          serviceBar.setDisable(false);
-          TranslateTransition slide = new TranslateTransition();
-          slide.setDuration(Duration.seconds(0.4));
-          slide.setNode(slider);
-
-          slide.setToY(490);
-          slide.play();
-
-          slider.setTranslateY(490);
-          slide.setOnFinished(
-              (ActionEvent e) -> {
-                serviceNav.setVisible(false);
-                closeServiceNav.setVisible(true);
-                serviceNav.setDisable(true);
-                closeServiceNav.setDisable(false);
-              });
-        });
-
-    closeServiceNav.setOnMouseClicked(
-        event -> {
-          TranslateTransition slide = new TranslateTransition();
-          slide.setDuration(Duration.seconds(0.4));
-          slide.setNode(slider);
-          slide.setToY(0);
-          slide.play();
-
-          slider.setTranslateY(0);
-
-          slide.setOnFinished(
-              (ActionEvent e) -> {
-                serviceNav.setVisible(true);
-                closeServiceNav.setVisible(false);
-                serviceNav.setDisable(false);
-                closeServiceNav.setDisable(true);
-                serviceBar.setVisible(false);
-                serviceBar.setDisable(true);
-              });
         });
 
     List<String> nodeNames =
--- src/main/java/edu/wpi/fishfolk/controllers/RootController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/RootController.java
@@ -1,8 +1,91 @@
 package edu.wpi.fishfolk.controllers;
 
+import edu.wpi.fishfolk.navigation.Navigation;
+import edu.wpi.fishfolk.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.animation.TranslateTransition;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 public class RootController {
+  @FXML MFXButton signageNav;
+  @FXML MFXButton mealNav;
+  // @FXML MFXButton supplyNav;
+  @FXML MFXButton pathfindingNav;
+  @FXML MFXButton mapEditorNav;
+  @FXML AnchorPane serviceBar;
+  @FXML MFXButton serviceNav;
+
+  @FXML MFXButton exitButton;
+
+  @FXML MFXButton closeServiceNav;
+  @FXML AnchorPane slider;
+  // @FXML Text directionInstructions;
+  @FXML MFXButton viewFood;
+  @FXML MFXButton viewSupply, furnitureNav;
+
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    viewFood.setOnMouseClicked(event -> Navigation.navigate(Screen.VIEW_FOOD_ORDERS));
+    viewSupply.setOnMouseClicked(event -> Navigation.navigate(Screen.VIEW_SUPPLY_ORDERS));
+    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
+    mealNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FOOD_ORDER_REQUEST));
+    furnitureNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FURNITURE_REQUEST));
+    mapEditorNav.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
+    pathfindingNav.setOnMouseClicked(event -> Navigation.navigate(Screen.PATHFINDING));
+    exitButton.setOnMouseClicked(event -> System.exit(0));
+    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
+    // mealNav.setOnMouseClicked(event -> Navigation.navigate(Screen.FOOD_ORDER_REQUEST));
+    // officeNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SUPPLIES_REQUEST));
+    mapEditorNav.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDITOR));
+    pathfindingNav.setOnMouseClicked(event -> Navigation.navigate(Screen.PATHFINDING));
+    exitButton.setOnMouseClicked(event -> System.exit(0));
+
+    closeServiceNav.setVisible(false);
+    closeServiceNav.setDisable(true);
+
+    serviceNav.setOnMouseClicked(
+        event -> {
+          serviceBar.setVisible(true);
+          serviceBar.setDisable(false);
+          TranslateTransition slide = new TranslateTransition();
+          slide.setDuration(Duration.seconds(0.4));
+          slide.setNode(slider);
+
+          slide.setToY(490);
+          slide.play();
+
+          slider.setTranslateY(490);
+          slide.setOnFinished(
+              (ActionEvent e) -> {
+                serviceNav.setVisible(false);
+                closeServiceNav.setVisible(true);
+                serviceNav.setDisable(true);
+                closeServiceNav.setDisable(false);
+              });
+        });
+
+    closeServiceNav.setOnMouseClicked(
+        event -> {
+          TranslateTransition slide = new TranslateTransition();
+          slide.setDuration(Duration.seconds(0.4));
+          slide.setNode(slider);
+          slide.setToY(0);
+          slide.play();
+
+          slider.setTranslateY(0);
+
+          slide.setOnFinished(
+              (ActionEvent e) -> {
+                serviceNav.setVisible(true);
+                closeServiceNav.setVisible(false);
+                serviceNav.setDisable(false);
+                closeServiceNav.setDisable(true);
+                serviceBar.setVisible(false);
+                serviceBar.setDisable(true);
+              });
+        });
+  }
 }
