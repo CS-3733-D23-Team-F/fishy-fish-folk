fd34a0eaa78b3e326e0dda517cab97ed5fc4f744
lbadler <lbadler@wpi.edu>
2023-04-17 00:21:37 -0400

    Added buttons, ability to switch between menu and cart view

--- src/main/java/edu/wpi/fishfolk/ui/NewFoodItem.java
+++ src/main/java/edu/wpi/fishfolk/ui/NewFoodItem.java
@@ -1,10 +1,11 @@
 package edu.wpi.fishfolk.ui;
 
 public class NewFoodItem {
-    String name;
-    byte quantity;
-    public NewFoodItem (String n, byte q) {
-        name = n;
-        quantity = q;
-    }
+  String name;
+  byte quantity;
+
+  public NewFoodItem(String n, byte q) {
+    name = n;
+    quantity = q;
+  }
 }
