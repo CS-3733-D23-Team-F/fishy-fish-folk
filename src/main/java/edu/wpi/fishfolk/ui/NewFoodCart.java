b0ccde0adf0639bda814c41f60168ad5e7eaf4e7
lbadler <lbadler@wpi.edu>
2023-04-17 05:51:33 -0400

    Cart view now Works

--- src/main/java/edu/wpi/fishfolk/ui/NewFoodCart.java
+++ src/main/java/edu/wpi/fishfolk/ui/NewFoodCart.java
@@ -2,9 +2,10 @@ package edu.wpi.fishfolk.ui;
 
 import java.util.ArrayList;
 import java.util.List;
+import lombok.Getter;
 
 public class NewFoodCart {
-  List<quantityItem> items;
+  @Getter List<quantityItem> items;
   float totalPrice;
 
   public NewFoodCart() {
@@ -13,8 +14,8 @@ public class NewFoodCart {
   }
 
   public static class quantityItem {
-    NewFoodMenuItem item;
-    byte quantity;
+    @Getter NewFoodMenuItem item;
+    @Getter byte quantity;
 
     quantityItem(NewFoodMenuItem i) {
       item = i;
@@ -30,13 +31,30 @@ public class NewFoodCart {
       }
     }
     items.add(new quantityItem(item));
+    totalPrice += item.price;
   }
 
-  public List<NewFoodItem> getItems() {
+  public boolean remove(NewFoodMenuItem item) {
+    for (quantityItem qi : items) {
+      if (qi.item == item) {
+        qi.quantity--;
+        totalPrice -= item.price;
+        if (qi.quantity == 0) items.remove(qi);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public List<NewFoodItem> getSubmittableItems() {
     List<NewFoodItem> returnable = new ArrayList<NewFoodItem>();
     for (quantityItem qi : items) {
       returnable.add(new NewFoodItem(qi.item.name, qi.quantity));
     }
     return returnable;
+  }
+
+  public void removeAll(NewFoodMenuItem item) {
+    while (remove(item)) ; // wtf is this, I love it
   }
 }
