de35217e2e211efcbe17c4aa7765130de7e20235
TEspelien <trajanespelien@gmail.com>
2023-04-18 15:45:18 -0400

    move editor ui, doesnt yet update database

--- src/main/java/edu/wpi/fishfolk/controllers/MoveController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/MoveController.java
@@ -1,33 +1,31 @@
 package edu.wpi.fishfolk.controllers;
 
-import static edu.wpi.fishfolk.database.DataTableType.LOCATION;
-import static edu.wpi.fishfolk.database.DataTableType.MOVE;
-
-import edu.wpi.fishfolk.database.edit.InsertEdit;
-import edu.wpi.fishfolk.database.edit.RemoveEdit;
-import edu.wpi.fishfolk.database.edit.UpdateEdit;
+import edu.wpi.fishfolk.database.DAO.Observables.ObservableLocation;
+import edu.wpi.fishfolk.database.DAO.Observables.ObservableMove;
+import edu.wpi.fishfolk.database.TableEntry.Location;
+import edu.wpi.fishfolk.database.TableEntry.Move;
+import edu.wpi.fishfolk.database.TableEntry.TableEntryType;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXDatePicker;
 import io.github.palexdev.materialfx.controls.MFXTextField;
-import java.util.ArrayList;
-import java.util.List;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class MoveController extends AbsController {
-  @FXML MFXButton submit;
-  @FXML MFXButton cancel;
-  @FXML MFXButton delete;
-  @FXML MFXButton Update;
-  @FXML MFXButton submitMove;
-  @FXML MFXButton cancelMove;
-  @FXML MFXButton deleteMove;
-  @FXML MFXButton UpdateMove;
-  @FXML MFXTextField longname;
-  @FXML MFXTextField shortname;
-  @FXML MFXTextField type;
-  @FXML MFXTextField longnameMove;
-  @FXML MFXTextField ID;
-  @FXML MFXDatePicker date;
+
+  @FXML TableView<ObservableMove> movetable;
+  @FXML TableColumn<ObservableMove, Integer> nodeid;
+  @FXML TableColumn<ObservableMove, String> movelongname, date;
+
+  @FXML TableView<ObservableLocation> locationtable;
+  @FXML TableColumn<ObservableLocation, String> locationlongname, shortname, type;
+  @FXML MFXTextField nodetext, movelongnametext, datetext;
+  @FXML MFXButton submitmove;
+
+  private ObservableList<ObservableMove> observableMoves;
 
   public MoveController() {
     super();
@@ -35,67 +33,39 @@ public class MoveController extends AbsController {
 
   @FXML
   private void initialize() {
-    submit.setOnMouseClicked(
-        event -> insertToDatabase(longname.getText(), shortname.getText(), type.getText()));
-    cancel.setOnMouseClicked(event -> clear());
-    delete.setOnMouseClicked(event -> RemoveToDatabase(longname.getText()));
-    Update.setOnMouseClicked(
-        event -> UpdateToDatabase(longname.getText(), shortname.getText(), type.getText()));
-    submitMove.setOnMouseClicked(
-        event -> insertToDatabaseMove(ID.getText(), longnameMove.getText(), date.getText()));
-    cancelMove.setOnMouseClicked(event -> clearMove());
-    deleteMove.setOnMouseClicked(event -> RemoveToDatabaseMove(ID.getText()));
-    UpdateMove.setOnMouseClicked(
-        event -> UpdateToDatabaseMove(ID.getText(), longnameMove.getText(), date.getText()));
-  }
 
-  private void insertToDatabase(String longname, String shortname, String Type) {
-    ArrayList<String> data = new ArrayList<>(List.of(longname, shortname, Type));
-    InsertEdit insert = new InsertEdit(LOCATION, "longname", longname, data);
-    dbConnection.processEdit(insert);
-  }
+    nodeid.setCellValueFactory(new PropertyValueFactory<>("nodeid"));
+    movelongname.setCellValueFactory(new PropertyValueFactory<>("longname"));
+    date.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-  private void insertToDatabaseMove(String id, String longname, String date) {
-    ArrayList<String> data = new ArrayList<>(List.of(id, longname, date));
-    InsertEdit insert = new InsertEdit(MOVE, "longname", longname, data);
-    dbConnection.processEdit(insert);
-  }
+    locationlongname.setCellValueFactory(new PropertyValueFactory<>("longname"));
+    shortname.setCellValueFactory(new PropertyValueFactory<>("shortname"));
+    type.setCellValueFactory(new PropertyValueFactory<>("type"));
 
-  private void UpdateToDatabase(String longname, String shortname, String Type) {
-    UpdateEdit updateShort = new UpdateEdit(LOCATION, "longname", longname, "shortname", shortname);
-    UpdateEdit updateType = new UpdateEdit(LOCATION, "longname", longname, "type", Type);
+    observableMoves =
+        FXCollections.observableArrayList(
+            dbConnection.getAllEntries(TableEntryType.MOVE).stream()
+                .map(elt -> new ObservableMove((Move) elt))
+                .toList());
+    movetable.setItems(observableMoves);
 
-    dbConnection.processEdit(updateShort);
-    dbConnection.processEdit(updateType);
-  }
+    locationtable.setItems(
+        FXCollections.observableList(
+            dbConnection.getAllEntries(TableEntryType.LOCATION).stream()
+                .map(elt -> new ObservableLocation((Location) elt))
+                .toList()));
 
-  private void UpdateToDatabaseMove(String id, String longname, String date) {
-    UpdateEdit updateName = new UpdateEdit(MOVE, "ID", id, "longname", longname);
-    UpdateEdit updateDate = new UpdateEdit(MOVE, "ID", id, "date", date);
+    submitmove.setOnMouseClicked(
+        event -> {
+          Move newMove =
+              new Move(
+                  Integer.parseInt(nodetext.getText()),
+                  movelongnametext.getText(),
+                  ObservableMove.parseDate(datetext.getText()));
 
-    dbConnection.processEdit(updateName);
-    dbConnection.processEdit(updateDate);
-  }
+          observableMoves.addAll(new ObservableMove(newMove));
 
-  private void RemoveToDatabase(String longname) {
-    RemoveEdit removeName = new RemoveEdit(LOCATION, "longname", longname);
-    dbConnection.processEdit(removeName);
-  }
-
-  private void RemoveToDatabaseMove(String id) {
-    RemoveEdit removeName = new RemoveEdit(MOVE, "id", id);
-    dbConnection.processEdit(removeName);
-  }
-
-  private void clear() {
-    longname.setText("");
-    shortname.setText("");
-    type.setText("");
-  }
-
-  private void clearMove() {
-    longnameMove.setText("");
-    ID.setText("");
-    date.setText("");
+          dbConnection.insertEntry(newMove);
+        });
   }
 }
