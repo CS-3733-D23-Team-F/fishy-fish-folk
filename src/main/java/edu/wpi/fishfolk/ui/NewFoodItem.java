aab7718c4de035d100eb7aab7893e9ed8f38bac7
lbadler <lbadler@wpi.edu>
2023-04-17 21:59:27 -0400

    added getters to NewFoodItem

--- src/main/java/edu/wpi/fishfolk/ui/NewFoodItem.java
+++ src/main/java/edu/wpi/fishfolk/ui/NewFoodItem.java
@@ -1,7 +1,11 @@
 package edu.wpi.fishfolk.ui;
 
+import lombok.Getter;
+
 public class NewFoodItem {
+  @Getter
   String name;
+  @Getter
   int quantity;
 
   public NewFoodItem(String n, int q) {
