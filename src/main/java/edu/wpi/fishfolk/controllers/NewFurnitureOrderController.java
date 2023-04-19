0fc1f084f3826dcda17970f618b47961e4e3e33a
lbadler <lbadler@wpi.edu>
2023-04-18 23:12:28 -0400

    fixed a but in FurnitureOrder

--- src/main/java/edu/wpi/fishfolk/controllers/NewFurnitureOrderController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewFurnitureOrderController.java
@@ -158,7 +158,7 @@ public class NewFurnitureOrderController extends AbsController {
 
   // getDate() returns the date. idk what the date function looked like as a string i was curious
   public LocalDateTime getDate() {
-    return deliveryDate.getCurrentDate().atStartOfDay();
+    return deliveryDate.getValue().atStartOfDay();
   }
 
   // setItemToRadios() checks the status of all of the buttons and sets the item in the current
