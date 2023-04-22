88d61fdbf8342fde83fcf6559d348333ed107b75
jpantojas888 <jpantojas888@gmail.com>
2023-04-22 18:26:51 -0400

    Added the cancel button functionality and created the init.

--- src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
@@ -1,3 +1,21 @@
 package edu.wpi.fishfolk.controllers;
 
-public class NewConferenceController extends AbsController {}
+import edu.wpi.fishfolk.navigation.Navigation;
+import edu.wpi.fishfolk.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class NewConferenceController extends AbsController {
+    @FXML MFXButton confClearButton;
+    @FXML MFXButton confCancelButton;
+    @FXML MFXButton confSubmitButton;
+
+    public NewConferenceController(){
+        super();
+    }
+
+    @FXML public void init(){
+        confCancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    }
+
+}
