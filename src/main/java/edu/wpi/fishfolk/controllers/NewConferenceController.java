97ed47789ecd0dc9c2c1867d0db762790addd4d7
jpantojas888 <jpantojas888@gmail.com>
2023-04-24 17:50:28 -0400

    Added some options to the drop downs

--- src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
@@ -26,19 +26,58 @@ public class NewConferenceController extends AbsController {
 
   @FXML
   public void initialize() {
-
+    addDropdownOptions();
     confCancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     confClearButton.setOnMouseClicked(event -> clearFields());
   }
 
   public void clearFields() {
-    startTimeDrop.setValue(null);
-    startAMPMDrop.setValue(null);
-    endAMPMDrop.setValue(null);
-    endTimeDrop.setValue(null);
+    startTimeDrop.setValue("TIME");
+    startAMPMDrop.setValue("AM/PM");
+    endAMPMDrop.setValue("AM/PM");
+    endTimeDrop.setValue("TIME");
     recurringDrop.setValue(null);
     numAttnBox.clear();
     notesBox.clear();
     nameBox.clear();
+  }
+
+  /** Initializing the dropdowns so they have all the required options. */
+  public void addDropdownOptions() {
+    // Dropdown options for the AM/PM box on start time
+    startAMPMDrop.getItems().add("AM");
+    startAMPMDrop.getItems().add("PM");
+
+    // Dropdown options for the AM/PM box on end time
+    endAMPMDrop.getItems().add("AM");
+    endAMPMDrop.getItems().add("PM");
+
+    // Dropdown options for the time box on start time
+    startTimeDrop.getItems().add("1:00");
+    startTimeDrop.getItems().add("2:00");
+    startTimeDrop.getItems().add("3:00");
+    startTimeDrop.getItems().add("4:00");
+    startTimeDrop.getItems().add("5:00");
+    startTimeDrop.getItems().add("6:00");
+    startTimeDrop.getItems().add("7:00");
+    startTimeDrop.getItems().add("8:00");
+    startTimeDrop.getItems().add("9:00");
+    startTimeDrop.getItems().add("10:00");
+    startTimeDrop.getItems().add("11:00");
+    startTimeDrop.getItems().add("12:00");
+
+    // Dropdown options for the time box on end time
+    endTimeDrop.getItems().add("1:00");
+    endTimeDrop.getItems().add("2:00");
+    endTimeDrop.getItems().add("3:00");
+    endTimeDrop.getItems().add("4:00");
+    endTimeDrop.getItems().add("5:00");
+    endTimeDrop.getItems().add("6:00");
+    endTimeDrop.getItems().add("7:00");
+    endTimeDrop.getItems().add("8:00");
+    endTimeDrop.getItems().add("9:00");
+    endTimeDrop.getItems().add("10:00");
+    endTimeDrop.getItems().add("11:00");
+    endTimeDrop.getItems().add("12:00");
   }
 }
