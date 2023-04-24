82dd175b2acc5941bdd5e41ce2c344ae55802f67
jpantojas888 <jpantojas888@gmail.com>
2023-04-24 18:56:05 -0400

    Created the recurring enum and DISCOVERED INLINE THANKS QUI.

--- src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
@@ -2,23 +2,23 @@ package edu.wpi.fishfolk.controllers;
 
 import edu.wpi.fishfolk.navigation.Navigation;
 import edu.wpi.fishfolk.navigation.Screen;
+import edu.wpi.fishfolk.ui.Recurring;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
+import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import java.util.ArrayList;
 import javafx.fxml.FXML;
 
 public class NewConferenceController extends AbsController {
-  @FXML MFXButton confClearButton;
-  @FXML MFXButton confCancelButton;
-  @FXML MFXButton confSubmitButton;
-  @FXML MFXFilterComboBox startTimeDrop;
-  @FXML MFXFilterComboBox endTimeDrop;
-  @FXML MFXFilterComboBox startAMPMDrop;
-  @FXML MFXFilterComboBox endAMPMDrop;
-  @FXML MFXFilterComboBox recurringDrop;
-  @FXML MFXTextField numAttnBox;
-  @FXML MFXTextField notesBox;
-  @FXML MFXTextField nameBox;
+  @FXML MFXButton confClearButton, confCancelButton, confSubmitButton;
+  @FXML MFXFilterComboBox startTimeDrop, endTimeDrop, startAMPMDrop, endAMPMDrop, recurringDrop;
+  @FXML MFXTextField numAttnBox, notesBox, nameBox;
+  @FXML MFXRectangleToggleNode rec1;
+
+  ArrayList<String> AMPM = new ArrayList<>();
+  ArrayList<String> times = new ArrayList<>();
+  ArrayList<Recurring> recurring = new ArrayList<>();
 
   public NewConferenceController() {
     super();
@@ -32,10 +32,10 @@ public class NewConferenceController extends AbsController {
   }
 
   public void clearFields() {
-    startTimeDrop.setValue("TIME");
-    startAMPMDrop.setValue("AM/PM");
-    endAMPMDrop.setValue("AM/PM");
-    endTimeDrop.setValue("TIME");
+    startTimeDrop.setValue(null);
+    startAMPMDrop.setValue(null);
+    endAMPMDrop.setValue(null);
+    endTimeDrop.setValue(null);
     recurringDrop.setValue(null);
     numAttnBox.clear();
     notesBox.clear();
@@ -44,40 +44,41 @@ public class NewConferenceController extends AbsController {
 
   /** Initializing the dropdowns so they have all the required options. */
   public void addDropdownOptions() {
-    // Dropdown options for the AM/PM box on start time
-    startAMPMDrop.getItems().add("AM");
-    startAMPMDrop.getItems().add("PM");
 
-    // Dropdown options for the AM/PM box on end time
-    endAMPMDrop.getItems().add("AM");
-    endAMPMDrop.getItems().add("PM");
+    // Dropdown options getting added to an arraylist called AMPM
+    AMPM.add("AM");
+    AMPM.add("PM");
 
-    // Dropdown options for the time box on start time
-    startTimeDrop.getItems().add("1:00");
-    startTimeDrop.getItems().add("2:00");
-    startTimeDrop.getItems().add("3:00");
-    startTimeDrop.getItems().add("4:00");
-    startTimeDrop.getItems().add("5:00");
-    startTimeDrop.getItems().add("6:00");
-    startTimeDrop.getItems().add("7:00");
-    startTimeDrop.getItems().add("8:00");
-    startTimeDrop.getItems().add("9:00");
-    startTimeDrop.getItems().add("10:00");
-    startTimeDrop.getItems().add("11:00");
-    startTimeDrop.getItems().add("12:00");
+    // Adding the arraylist of AM or PM to the dropdowns so they can be selected
+    startAMPMDrop.getItems().addAll(AMPM);
+    endAMPMDrop.getItems().addAll(AMPM);
 
-    // Dropdown options for the time box on end time
-    endTimeDrop.getItems().add("1:00");
-    endTimeDrop.getItems().add("2:00");
-    endTimeDrop.getItems().add("3:00");
-    endTimeDrop.getItems().add("4:00");
-    endTimeDrop.getItems().add("5:00");
-    endTimeDrop.getItems().add("6:00");
-    endTimeDrop.getItems().add("7:00");
-    endTimeDrop.getItems().add("8:00");
-    endTimeDrop.getItems().add("9:00");
-    endTimeDrop.getItems().add("10:00");
-    endTimeDrop.getItems().add("11:00");
-    endTimeDrop.getItems().add("12:00");
+    // Dropdown options getting added to an arraylist called times
+    times.add("1:00");
+    times.add("2:00");
+    times.add("3:00");
+    times.add("4:00");
+    times.add("5:00");
+    times.add("6:00");
+    times.add("7:00");
+    times.add("8:00");
+    times.add("9:00");
+    times.add("10:00");
+    times.add("11:00");
+    times.add("12:00");
+
+    // Adding the arraylist of times to the dropdowns so they can be selected
+    startTimeDrop.getItems().addAll(times);
+    endTimeDrop.getItems().addAll(times);
+
+    // Adding all possible recurring options to an arraylist
+    recurring.add(Recurring.NEVER);
+    recurring.add(Recurring.DAILY);
+    recurring.add(Recurring.WEEKLY);
+    recurring.add(Recurring.MONTHLY);
+    recurring.add(Recurring.YEARLY);
+
+    // Adding the arraylist to the dropdown box
+    recurringDrop.getItems().addAll(recurring);
   }
 }
