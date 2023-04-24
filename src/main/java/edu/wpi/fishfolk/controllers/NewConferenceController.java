2f5e427564c09a70e3c83203b80a7b2e8deceeb7
jpantojas888 <jpantojas888@gmail.com>
2023-04-24 14:50:52 -0400

    Added fxids for everything

--- src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
@@ -3,12 +3,23 @@ package edu.wpi.fishfolk.controllers;
 import edu.wpi.fishfolk.navigation.Navigation;
 import edu.wpi.fishfolk.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXFilterComboBox;
+import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
 
 public class NewConferenceController extends AbsController {
   @FXML MFXButton confClearButton;
   @FXML MFXButton confCancelButton;
   @FXML MFXButton confSubmitButton;
+  @FXML MFXFilterComboBox startTimeDrop;
+  @FXML MFXFilterComboBox endTimeDrop;
+  @FXML MFXFilterComboBox startAMPMDrop;
+  @FXML MFXFilterComboBox endAMPMDrop;
+  @FXML MFXFilterComboBox recurringDrop;
+  @FXML MFXTextField numAttnBox;
+  @FXML MFXTextField notesBox;
+  @FXML MFXTextField nameBox;
+
 
   public NewConferenceController() {
     super();
@@ -21,7 +32,5 @@ public class NewConferenceController extends AbsController {
     confClearButton.setOnMouseClicked(event -> clearFields());
   }
 
-  public void clearFields(){
-
-  }
+  public void clearFields() {}
 }
