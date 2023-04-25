0b4146a1d938521ac341d28e98769513a82c6a82
jpantojas888 <jpantojas888@gmail.com>
2023-04-24 21:27:32 -0400

    Clear is fully done now

--- src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/NewConferenceController.java
@@ -1,5 +1,6 @@
 package edu.wpi.fishfolk.controllers;
 
+import edu.wpi.fishfolk.SharedResources;
 import edu.wpi.fishfolk.navigation.Navigation;
 import edu.wpi.fishfolk.navigation.Screen;
 import edu.wpi.fishfolk.ui.Recurring;
@@ -9,15 +10,12 @@ import io.github.palexdev.materialfx.controls.MFXRectangleToggleNode;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import java.util.ArrayList;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 
 public class NewConferenceController extends AbsController {
   @FXML MFXButton confClearButton, confCancelButton, confSubmitButton;
   @FXML MFXFilterComboBox startTimeDrop, endTimeDrop, startAMPMDrop, endAMPMDrop, recurringDrop;
-  @FXML MFXTextField numAttnBox, nameBox;
-  @FXML
-  TextArea notesBox;
-  @FXML MFXRectangleToggleNode rec1;
+  @FXML MFXTextField numAttnBox, nameBox, notesBox;
+  @FXML MFXRectangleToggleNode rec1, rec2, rec3, rec4, rec5, rec6, rec7;
 
   ArrayList<String> AMPM = new ArrayList<>();
   ArrayList<String> times = new ArrayList<>();
@@ -32,17 +30,26 @@ public class NewConferenceController extends AbsController {
     addDropdownOptions();
     confCancelButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     confClearButton.setOnMouseClicked(event -> clearFields());
+    confSubmitButton.setOnMouseClicked(event -> attemptLogin());
+    nameBox.setText(SharedResources.getCurrentUser().getUsername());
   }
 
   public void clearFields() {
+    rec1.setSelected(false);
+    rec2.setSelected(false);
+    rec3.setSelected(false);
+    rec4.setSelected(false);
+    rec5.setSelected(false);
+    rec6.setSelected(false);
+    rec7.setSelected(false);
     startTimeDrop.setValue(null);
     startAMPMDrop.setValue(null);
     endAMPMDrop.setValue(null);
     endTimeDrop.setValue(null);
     recurringDrop.setValue(null);
     numAttnBox.clear();
-    notesBox.setText(null);
-    nameBox.clear();
+    notesBox.clear();
+    nameBox.setText(SharedResources.getCurrentUser().getUsername());
   }
 
   /** Initializing the dropdowns so they have all the required options. */
@@ -83,5 +90,19 @@ public class NewConferenceController extends AbsController {
 
     // Adding the arraylist to the dropdown box
     recurringDrop.getItems().addAll(recurring);
+  }
+
+  public void attemptLogin() {
+    if (rec1.isSelected()
+        || rec2.isSelected()
+        || rec3.isSelected()
+        || rec4.isSelected()
+        || rec5.isSelected()
+        || rec6.isSelected()
+        || rec7.isSelected()) {
+      System.out.println("Sufficient fields filled");
+    } else {
+      System.out.println("Sufficient fields not filled");
+    }
   }
 }
