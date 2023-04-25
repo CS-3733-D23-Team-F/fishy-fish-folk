1bfb00f4c47ac2c3d9adfb243543cee60cd2e0d8
Samuel Rooney <the.sam.rooney@gmail.com>
2023-04-25 12:49:33 -0400

    switch signage navigator button to go to new signage page

--- src/main/java/edu/wpi/fishfolk/controllers/RootController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/RootController.java
@@ -52,7 +52,7 @@ public class RootController {
           Navigation.navigate(Screen.LOGIN);
         });
 
-    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
+    signageNav.setOnMouseClicked(event -> Navigation.navigate(Screen.NEW_SIGNAGE));
     exitButton.setOnMouseClicked(event -> System.exit(0));
     homeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     moveEditorNav.setOnMouseClicked(event -> Navigation.navigate(Screen.MOVE_EDITOR));
