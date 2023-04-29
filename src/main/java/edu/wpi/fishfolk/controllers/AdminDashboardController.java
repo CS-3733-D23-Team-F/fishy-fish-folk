9df13ea48e6f6540188059283c24f67d4d5d6c45
L0uisPham <knpham@wpi.edu>
2023-04-29 15:38:58 -0400

    Updated some stuffs inside table views, still couldn't figure out how to implement text wrapping or any other alternatives

--- src/main/java/edu/wpi/fishfolk/controllers/AdminDashboardController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/AdminDashboardController.java
@@ -5,6 +5,7 @@ import static edu.wpi.fishfolk.controllers.AbsController.dbConnection;
 import edu.wpi.fishfolk.Fapp;
 import edu.wpi.fishfolk.database.DAO.Observables.*;
 import edu.wpi.fishfolk.database.TableEntry.*;
+import edu.wpi.fishfolk.database.TableEntry.Alert;
 import edu.wpi.fishfolk.navigation.Navigation;
 import edu.wpi.fishfolk.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -24,10 +25,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.ChoiceBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.*;
@@ -90,6 +88,7 @@ public class AdminDashboardController {
   public void initialize() {
     ArrayList<Move> moves = (ArrayList<Move>) dbConnection.getAllEntries(TableEntryType.MOVE);
     setTable();
+
     outstandingFilter.setOnMouseClicked(
         event -> {
           setOutstandingTable();
@@ -414,9 +413,12 @@ public class AdminDashboardController {
     supplyTable.setEditable(true);
     flowerTable.setEditable(true);
 
-    foodTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+    // foodTable.setColumnResizePolicy(TableView.UNCONSTRAINED_RESIZE_POLICY);
+
+    // foodTable.autosize();
 
     foodTable.setItems(getFoodOrderRows());
+
     supplyTable.setItems(getSupplyOrderRows());
     furnitureTable.setItems(getFurnitureOrderRows());
     flowerTable.setItems(getFlowerOrderRows());
