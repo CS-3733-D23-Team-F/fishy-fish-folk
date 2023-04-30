e2be0859afd17d9ff2e79712e56be70baf035c75
L0uisPham <knpham@wpi.edu>
2023-04-30 12:10:54 -0400

    IT WORKED

--- src/main/java/edu/wpi/fishfolk/controllers/LoadingScreenController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/LoadingScreenController.java
@@ -23,8 +23,8 @@ public class LoadingScreenController implements Initializable {
   public Label loading;
   public static Label loadings;
 
-  public ImageView evolved, evolving, defaultForm;
-  public static ImageView evolvesf, evolvingf, defaultFormf;
+  public ImageView evolved, evolving, defaultForm, evolvedA;
+  public static ImageView evolvesf, evolvingf, defaultFormf, evolvedf;
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -32,6 +32,7 @@ public class LoadingScreenController implements Initializable {
     evolvesf = evolved;
     evolvingf = evolving;
     defaultFormf = defaultForm;
+    evolvedf = evolvedA;
   }
 
   public String checkFunctions() {
@@ -39,7 +40,7 @@ public class LoadingScreenController implements Initializable {
     Thread t1 =
         new Thread(
             () -> {
-              message[0] = "What?\nMagikarp is evolving!";
+              message[0] = "What?\nMAGIKARP is evolving!";
               Platform.runLater(() -> loadings.setText(message[0]));
               try {
                 Thread.sleep(10000);
@@ -52,40 +53,76 @@ public class LoadingScreenController implements Initializable {
     Thread t2 =
         new Thread(
             () -> {
-              message[0] = "Magikarp evolved into Fishy-Fish-Folks";
+              message[0] = "What?\nMAGIKARP is evolving!";
+              Timeline timeline = new Timeline();
+              timeline.setCycleCount(1);
+              Timeline timeline2 = new Timeline();
+              timeline2.setCycleCount(1);
               Platform.runLater(
                   () -> {
                     loadings.setText(message[0]);
 
-                    Duration zoomDuration = Duration.seconds(1);
-                    Timeline timeline = new Timeline();
-                    timeline.setCycleCount(Timeline.INDEFINITE);
+                    evolvesf.setVisible(false);
+                    evolvingf.setVisible(false);
 
-                    FadeTransition fadeTransition = new FadeTransition(Duration.seconds(1), defaultFormf);
-                    fadeTransition.setFromValue(0);
-                    fadeTransition.setToValue(1);
+                    Duration zoomDuration = Duration.seconds(0.5);
 
-                    double zoomFactor = 1.2;
+                    FadeTransition fadeTransition =
+                        new FadeTransition(Duration.seconds(0.5), defaultFormf);
+                    fadeTransition.setFromValue(1);
+                    fadeTransition.setToValue(0);
+                    KeyValue keyValue = new KeyValue(defaultFormf.opacityProperty(), 0.0);
+                    KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.5), keyValue);
+                    Timeline timeline1 = new Timeline(keyFrame);
+                    ParallelTransition parallelTransition =
+                        new ParallelTransition(fadeTransition, timeline1);
+                    parallelTransition.play();
+
+                    double zoomFactor = 1;
+
                     KeyValue zoomOutValue1 =
-                        new KeyValue(evolvingf.scaleXProperty(), 1 / zoomFactor);
+                        new KeyValue(evolvingf.scaleXProperty(), 1 - zoomFactor);
                     KeyValue zoomOutValue2 =
-                        new KeyValue(evolvesf.scaleYProperty(), 1 / zoomFactor);
-                    KeyValue zoomInValue1 = new KeyValue(evolvingf.scaleXProperty(), zoomFactor);
+                        new KeyValue(evolvingf.scaleYProperty(), 1 - zoomFactor);
+                    KeyValue zoomInValue1 = new KeyValue(evolvesf.scaleXProperty(), zoomFactor);
                     KeyValue zoomInValue2 = new KeyValue(evolvesf.scaleYProperty(), zoomFactor);
+
+                    KeyValue zoomOutValue3 =
+                        new KeyValue(evolvesf.scaleXProperty(), 1 - zoomFactor);
+                    KeyValue zoomOutValue4 =
+                        new KeyValue(evolvesf.scaleYProperty(), 1 - zoomFactor);
+                    KeyValue zoomInValue3 = new KeyValue(evolvingf.scaleXProperty(), zoomFactor);
+                    KeyValue zoomInValue4 = new KeyValue(evolvingf.scaleYProperty(), zoomFactor);
 
                     KeyFrame zoomOutFrame =
                         new KeyFrame(zoomDuration, zoomOutValue1, zoomOutValue2);
                     KeyFrame zoomInFrame =
                         new KeyFrame(zoomDuration.multiply(2), zoomInValue1, zoomInValue2);
 
+                    KeyFrame zoomOutFrame2 =
+                        new KeyFrame(zoomDuration, zoomOutValue3, zoomOutValue4);
+                    KeyFrame zoomInFrame2 =
+                        new KeyFrame(zoomDuration.multiply(2), zoomInValue3, zoomInValue4);
+
                     timeline.getKeyFrames().addAll(zoomOutFrame, zoomInFrame);
 
+                    timeline2.getKeyFrames().addAll(zoomOutFrame2, zoomInFrame2);
 
-                    timeline.play();
+                    parallelTransition.setOnFinished(
+                        event -> {
+                          evolvesf.setVisible(true);
+                          evolvingf.setVisible(true);
+                          timeline.play();
+                          timeline.setOnFinished(event1 -> timeline2.play());
+                          timeline2.setOnFinished(event1 -> timeline.play());
+                        });
                   });
               try {
                 Thread.sleep(10000);
-
+                timeline2.stop();
+                timeline.stop();
+                evolvingf.setVisible(false);
+                evolvesf.setVisible(false);
               } catch (Exception e) {
                 e.printStackTrace();
               }
@@ -137,24 +174,48 @@ public class LoadingScreenController implements Initializable {
                   });
             });
 
-    System.out.println(
-        "state: " + t3.getState() + " t1: " + t1.getState() + " t2 " + t2.getState());
+    Thread t4 =
+        new Thread(
+            () -> {
+              message[0] = "Congratulations! Your MAGIKARP\nevolved into FISHY-FISH-FOLK";
+              Platform.runLater(
+                  () -> {
+                    loadings.setText(message[0]);
+
+                    evolvedf.setVisible(true);
+                    evolvedf.toFront();
+                    ScaleTransition scaleTransition =
+                        new ScaleTransition(Duration.seconds(1), evolvedf);
+                    scaleTransition.setFromX(0);
+                    scaleTransition.setFromY(0);
+                    scaleTransition.setToX(1);
+                    scaleTransition.setToY(1);
+                    KeyValue keyValue = new KeyValue(evolvedf.opacityProperty(), 1.0);
+                    KeyFrame keyFrame = new KeyFrame(Duration.seconds(1), keyValue);
+                    Timeline timeline1 = new Timeline(keyFrame);
+                    ParallelTransition parallelTransition =
+                        new ParallelTransition(scaleTransition, timeline1);
+                    parallelTransition.play();
+                  });
+              try {
+                Thread.sleep(3000);
+
+              } catch (InterruptedException e) {
+                e.printStackTrace();
+              }
+            });
+
     try {
 
       t1.start();
       t3.start();
-      if (t1.getState() == Thread.State.WAITING) System.out.println("deadge");
-      System.out.println(
-          "state: " + t3.getState() + " t1: " + t1.getState() + " t2 " + t2.getState());
       t1.join();
-      System.out.println(
-          "state: " + t3.getState() + " t1: " + t1.getState() + " t2 " + t2.getState());
-
       t3.join();
       t2.start();
       t2.join();
-      System.out.println(
-          "state: " + t3.getState() + " t1: " + t1.getState() + " t2 " + t2.getState());
+      t4.start();
+      t4.join();
+
     } catch (InterruptedException e) {
       e.printStackTrace();
     }
