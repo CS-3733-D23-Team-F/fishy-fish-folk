963c213bc16a614c62f1d23cb7698fa860dc2795
L0uisPham <knpham@wpi.edu>
2023-04-30 21:19:47 -0400

    Killed the fish

--- src/main/java/edu/wpi/fishfolk/Main.java
+++ src/main/java/edu/wpi/fishfolk/Main.java
@@ -1,10 +1,11 @@
 package edu.wpi.fishfolk;
 
-import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Application;
 
 public class Main {
 
   public static void main(String[] args) {
-    LauncherImpl.launchApplication(Fapp.class, Preloader.class, args); // run ui
+    // LauncherImpl.launchApplication(Fapp.class, Preloader.class, args); // Launch the cool UI :(
+    Application.launch(Fapp.class, args);
   }
 }
