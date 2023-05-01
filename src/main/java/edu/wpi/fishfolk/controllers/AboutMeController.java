335dd370706d0a22a41b211f19b9e244d3f91f4a
Byrnezy <97705645+Byrnezy@users.noreply.github.com>
2023-04-30 20:03:35 -0400

    made the pages smaller for everyone that has given me the information for the about me pages

--- src/main/java/edu/wpi/fishfolk/controllers/AboutMeController.java
+++ src/main/java/edu/wpi/fishfolk/controllers/AboutMeController.java
@@ -1,15 +1,19 @@
 package edu.wpi.fishfolk.controllers;
 
-import edu.wpi.fishfolk.Fapp;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import lombok.Getter;
+import lombok.Setter;
 
-public class AboutMeController {
-
+public class AboutMeController // extends Application
+ {
+  @Setter @Getter private static Stage aboutpages;
+  @Setter @Getter private static AnchorPane anchor;
   @FXML Label AboutM;
   @FXML MFXButton Jon;
   @FXML MFXButton Meg;
@@ -22,14 +26,17 @@ public class AboutMeController {
   @FXML MFXButton Trajan;
   @FXML MFXButton Bernhardt;
   @FXML MFXButton Brendan;
-  @FXML AnchorPane AboutMe;
+  @FXML VBox AboutMe;
 
   @FXML
   public void initialize() {
 
+    Jon.setOnMouseClicked(event -> PopPage());
+
+    /*
     // none of this works
-    final FXMLLoader JonPage =
-        new FXMLLoader(AboutMeController.class.getResource("views/popups/JonPage.fxml"));
+
+    final FXMLLoader JonPage = new FXMLLoader(AboutMeController.class.getResource("views/popups/JonPage.fxml"));
     FXMLLoader MaxPage = new FXMLLoader(getClass().getResource("MaxPage.fxml"));
     FXMLLoader MegPage = new FXMLLoader(getClass().getResource("MegPage.fxml"));
     FXMLLoader TrajanPage = new FXMLLoader(getClass().getResource("TrajanPage.fxml"));
@@ -50,21 +57,91 @@ public class AboutMeController {
     Charlie.setOnMouseClicked(event -> PopPage(CharliePage));
     Sam.setOnMouseClicked(event -> PopPage(SamPage));
     Christian.setOnMouseClicked(event -> PopPage(ChristianPage));
-    Brendan.setOnMouseClicked(event -> PopPage(BrendanPage));
+    Brendan.setOnMouseClicked(event -> PopPage(BrendanPage)); */
   }
 
-  public void PopPage(FXMLLoader name) {
+  /*public void start(Stage stage) throws IOException
+  {
+    // set title for the stage
+    stage.setTitle("Creating popup");
 
+    // create a tile pane
+    AnchorPane tilepane = new AnchorPane();
+
+    // create a popup
+    Popup popup = new Popup();
+
+    final FXMLLoader JonPage = new FXMLLoader(AboutMeController.class.getResource("views/popups/JonPage.fxml"));
+    final AnchorPane popupPage = JonPage.load();
+    anchor = popupPage;
+
+    Scene scene = new Scene(popupPage);
+
+    aboutpages.setScene(scene);
+    aboutpages.setMaximized(true);
+    aboutpages.show();
+
+
+    // add the label
+    popup.getContent().;
+
+    // action event
+    EventHandler<ActionEvent> event =
+            new EventHandler<ActionEvent>() {
+
+              public void handle(ActionEvent e)
+              {
+                if (!popup.isShowing())
+                  popup.show(stage);
+                else
+                  popup.hide();
+              }
+            };
+
+    // add button
+    tilepane.getChildren().add(button);
+
+    // create a scene
+    Scene scene = new Scene(tilepane, 200, 200);
+
+    // set the scene
+    stage.setScene(scene);
+
+    stage.show();
+  }*/
+  // FXMLLoader name
+  public void PopPage() {
+
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
+      fxmlLoader.setLocation(AboutMeController.class.getResource("views/popups/JonPage.fxml"));
+
+      VBox vbox = fxmlLoader.load();
+
+      // AlertsController alertsController = fxmlLoader.getController();
+
+      // alertsController.closeAlert.setDisable(true);
+      // alertsController.closeAlert.setVisible(false);
+
+      AboutMe.getChildren().clear();
+      AboutMe.getChildren().add(vbox);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
+    /*
     try {
       FXMLLoader fxmlLoader = new FXMLLoader();
       fxmlLoader.setLocation(Fapp.class.getResource("views/popups/JonPage.fxml"));
 
       HBox namePane = fxmlLoader.load();
-
-      AboutMe.getChildren().clear();
-      AboutMe.getChildren().add(namePane);
+      Stage window = new Stage();
+      Scene scene = new Scene(namePane);
+      window.setScene(scene);
+      window.show();
+      // AboutMe.getChildren().clear();
+      // AboutMe.getChildren().add(namePane);
     } catch (Exception e) {
       System.out.println(e.getMessage());
-    }
+    }*/
   }
 }
